bfcf866596c2f59251e7315dd8b44c886f9b5118
salusKarabu <k.mariasalus@gmail.com>
2023-06-10 02:52:36 -0700

    convert a string into an integer

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,24 +1,29 @@
 #include "main.h"
 
 /**
- * char *_strcpy - a function that copies the string pointed to by src
- * @dest: copy to
- * @src: copy from
- * Return: string
+ * _atoi - convert a string into an integer.
+ *
+ * @s: the string to use.
+ *
+ * Return: integer.
  */
-char *_strcpy(char *dest, char *src)
-{
-	int l = 0;
-	int x = 0;
 
-	while (*(src + l) != '\0')
+int _atoi(char *s)
+{
+	int sign = 1, i = 0;
+	unsigned int res = 0;
+
+	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
 	{
-		l++;
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
 	}
-	for ( ; x < l ; x++)
+	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
 	{
-		dest[x] = src[x];
+		res = (res * 10) + (s[i] - '0');
+		i++;
 	}
-	dest[l] = '\0';
-	return (dest);
+	res *= sign;
+	return (res);
 }
